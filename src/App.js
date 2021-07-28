5422819af58f0c69cf4fa598e5189af7a9553d74
grace <grace.c.youngblood@gmail.com>
2021-07-27 22:19:31 -0400

    Basic skeleton for drag and drop.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from "react"
+import styled from "styled-components"
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+class App extends React.Component {
+  state = {
+    cards: [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+    ],
+  }
+
+  swapFromTo(begin, end) {
+    var cards = this.state.cards
+
+    if(begin < end)
+      this.setState({ cards:
+        cards.slice(0, begin)
+        .concat(cards.slice(begin + 1, end + 1))
+        .concat(cards[begin])
+        .concat(cards.slice(end + 1, cards.length))
+      })
+    else if(end < begin)
+      this.setState({ cards:
+        cards.slice(0, end)
+        .concat(cards[begin])
+        .concat(cards.slice(end, begin))
+        .concat(cards.slice(begin + 1, cards.length))
+      })
+  }
+
+  render = () => (
+    <DragDropContext
+      onDragEnd={(drop) => this.swapFromTo(drop.source.index, drop.destination.index)}
+    >
+
+      <div>
+
+        <Droppable
+          droppableId="droppable-phase"
+          type="PHASE"
+          direction="horizontal"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {(provided, snapshot) => (
+          <Container
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {provided.placeholder}
+
+          {this.state.cards.map((card, i) => (
+            <Draggable draggableId={`draggable-phase-${i}`} index={i}>
+              {(provided, snapshot) => (
+                <Card
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                >
+                  {card}
+                </Card>
+              )}
+            </Draggable>
+          ))}
+
+          </Container>
+        )}
+        </Droppable>
+
+      </div>
+    </DragDropContext>
+  )
 }
+
+var Container = styled.div`
+`
+
+var Card = styled.div`
+`
 
 export default App;
